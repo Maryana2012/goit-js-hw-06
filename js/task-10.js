fe5cabd2c24c017780badafbe4ee061123a59532
Maryana2012 <maryana.oryshchuk@gmail.com>
2023-02-26 16:22:10 +0200

    10 завдання допрацьовую

--- js/task-10.js
+++ js/task-10.js
@@ -3,46 +3,47 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-// debugger
+
 const inputEl = document.querySelector('input');
 const BtnEls = document.querySelectorAll('button');
 const createBtnEl = BtnEls[0];
 const destroyBtnEl = BtnEls[1]; 
 const boxesEl = document.querySelector('#boxes');
 
-// debugger
 
-inputEl.addEventListener('input', (event) => {
-  const amount = event.currentTarget.value;
-   console.log(event.currentTarget.value);  
-  }) 
+createBtnEl.addEventListener('click', handleBoxes);
+// createBtnEl.addEventListener('click', () => {
+//   boxesEl.remove();
+// });
+
+function handleBoxes() {
+
+  inputEl.addEventListener('input', (event) => {
+     
+     const amount = Number(event.currentTarget.value);
+
+    }) 
+    createBoxes(amount);
+}  
+
+function createBoxes(number) {
   
-// const amount = inputEl.value;
-
-// console.log(inputEl);
-// console.log(inputEl.value);
-// console.log(amount);
-
-// createBtnEl.addEventListener('click', createBoxes);
-
-
-// function createBoxes(amount) {
+  let width = 0;
+  let height = 0; 
   
-//   let width = 0;
-//   let height = 0; 
-  
-//   for (let i = 1; i <= amount; i += 1){
+  for (let i = 1; i <= number; i += 1){
     
-//     width += 10;
-//     height += 10;
+    width += 10;
+    height += 10;
     
-//     const boxEl = document.createElement('div');
+    const boxEl = document.createElement('div');
     
-//     boxEl.style.width = `${width}px`;
-//     boxEl.style.height = `${height}px`;
-//     boxEl.style.backgroundColor = getRandomHexColor();
+    boxEl.style.width = `${width}px`;
+    boxEl.style.height = `${height}px`;
+    boxEl.style.backgroundColor = getRandomHexColor();
     
-//     boxesEl.append(boxEl);
-//   }
-//   return boxesEl; 
-// }
+    boxesEl.append(boxEl);
+  }
+  return boxesEl; 
+}
+
